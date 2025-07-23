3a065ca980c64aa2c7cb18a1e2fba06e8a02b3ca
suraj123 <surajmittal432@gmail.com>
2025-07-23 16:55:42 +0530

    stripe new

--- server/controllers/stripewebhooks.js
+++ server/controllers/stripewebhooks.js
@@ -1,5 +1,5 @@
 import stripe from "stripe";
-import Booking from "../models/Booking.jss";
+import Booking from "../models/Booking.js";
 
 
 export const stripeWebhooks = async (request, response) => {
